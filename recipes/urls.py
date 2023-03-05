1ad356d59d4562d51685f9b09daed216bfeb9d3b
Nkululeko <nkululeko.cele@gmail.com>
2023-03-05 13:40:35 +0200

    Write CSS and HTML

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('apple-cake-recipe/', views.apple_cake_recipe, name='apple_cake_recipe'),
+    path('carrot-cake-recipe/', views.carrot_cake_recipe,
+         name="carrot_cake_recipe"),
+    path('pumpkin-cake-recipe/', views.pumpkin_cake_recipe,
+         name='pumpkin_cake_recipe'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'recipes/index.html')
+
+
+def about(request):
+    return render(request, 'recipes/about.html')
+
+
+def apple_cake_recipe(request):
+    return render(request, 'recipes/apple_cake_recipe.html')
+
+
+def carrot_cake_recipe(request):
+    return render(request, 'recipes/carrot_cake_recipe.html')
+
+
+def pumpkin_cake_recipe(request):
+    return render(request, 'recipes/pumpkin_cake_recipe.html')
