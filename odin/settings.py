14eb919d1ce746797c4a483dd3ead7a1b285b0eb
Nkululeko <nkululeko.cele@gmail.com>
2023-03-05 10:55:42 +0200

    Update local changes

--- odin/settings.py
+++ odin/settings.py
@@ -125,10 +125,8 @@ USE_TZ = True
 
 STATIC_URL = 'recipes/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'recipes/static',
+    'recipes/static'
 ]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,1 @@
 from django.contrib import admin
-from .models import Recipe, AboutPage, HomePage
-
-admin.site.register([Recipe, AboutPage, HomePage])
